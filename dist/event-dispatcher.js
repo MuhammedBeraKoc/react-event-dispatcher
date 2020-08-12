2a912f897d98882167037395a87c6739c579eb6d
Bera <berakoc97@gmail.com>
2020-08-12 20:56:32 +0300

    📦 NEW: Built bundle file for the new version.

--- dist/event-dispatcher.js
+++ dist/event-dispatcher.js
@@ -91,6 +91,8 @@ module.exports =
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -103,6 +105,26 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToAr
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -112,26 +134,43 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var EventDispatcher = /*#__PURE__*/function () {
+  /**
+   * A static variable for storing events
+   * It is shared across all components
+   * Yet it is hidden from them
+   * So this variable is private
+   */
   function EventDispatcher() {
     _classCallCheck(this, EventDispatcher);
+
+    this.KeyNotFoundError = /*#__PURE__*/function (_Error) {
+      _inherits(_class, _Error);
+
+      var _super = _createSuper(_class);
+
+      function _class(message) {
+        var _this;
+
+        _classCallCheck(this, _class);
+
+        _this = _super.call(this, message);
+        _this.name = 'KeyNotFoundError';
+        return _this;
+      }
+
+      return _class;
+    }( /*#__PURE__*/_wrapNativeSuper(Error));
   }
+  /**
+   * Dispatches the given function set to the __$.
+   * @param {string} key The string representation of the components name
+   * @param {Function[]} $functionSet A set of functions which is to be dispatched
+   * @return {void}
+   */
+
 
   _createClass(EventDispatcher, [{
     key: "dispatch",
-
-    /**
-     * A static variable for storing events
-     * It is shared across all components
-     * Yet it is hidden from them
-     * So this variable is private
-     */
-
-    /**
-     * Dispatches the given function set to the __$.
-     * @param {string} key The string representation of the components name
-     * @param {Function[]} $functionSet A set of functions which is to be dispatched
-     * @return {void}
-     */
     value: function dispatch(key) {
       for (var _len = arguments.length, $functionSet = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
         $functionSet[_key - 1] = arguments[_key];
@@ -150,13 +189,15 @@ var EventDispatcher = /*#__PURE__*/function () {
   }, {
     key: "getOne",
     value: function getOne(key) {
+      var _this2 = this;
+
       var index = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
       if (EventDispatcher.__$.has(key)) {
         return EventDispatcher.__$.get(key)[index];
       } else {
         return function () {
-          throw new Error('Key has not been found');
+          throw new _this2.KeyNotFoundError('Key has not been found.');
         };
       }
     }
@@ -170,11 +211,13 @@ var EventDispatcher = /*#__PURE__*/function () {
   }, {
     key: "getAll",
     value: function getAll(key) {
+      var _this3 = this;
+
       if (EventDispatcher.__$.has(key)) {
         return EventDispatcher.__$.get(key);
       } else {
         return function () {
-          throw new Error('Key has not been found');
+          throw new _this3.KeyNotFoundError('Key has not been found.');
         };
       }
     }
@@ -213,6 +256,19 @@ var EventDispatcher = /*#__PURE__*/function () {
       }
 
       return returnValues;
+    }
+    /**
+     * Removes the given key from key map.
+     * Warning: This method is meant to be used only in componentWillUnmount
+     * @param {string} key The string representation of the components name
+     */
+
+  }, {
+    key: "deleteKey",
+    value: function deleteKey(key) {
+      if (!EventDispatcher.__$["delete"](key)) {
+        throw new this.KeyNotFoundError('Key has not been found.');
+      }
     }
     /**
      * Clears the event map __$.
